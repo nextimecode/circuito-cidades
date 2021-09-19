bc4f7869c917e1146be9fbd60fa4eba0bc1ba42c
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-09-19 17:37:07 -0300

    build: header

---
+++ src/pages/cidades.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cidades = () => {
+  return (
+    <h1>Cidades</h1>
+  )
+}
+
+export default Cidades
---
+++ src/pages/documentos.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Documentos = () => {
+  return (
+    <h1>Documentos</h1>
+  )
+}
+
+export default Documentos
