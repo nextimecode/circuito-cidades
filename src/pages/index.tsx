98b3db75ea5b336a92b27f7f84e4d59c775ab899
Pedro Duarte <pedrohlduarte@gmail.com>
2021-09-23 18:12:40 -0300

    fix: nome dos componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,11 @@
 
 import React from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import NextButton from '../components/atoms/nextButton'
+import TemplateHome from '../components/templates/templateHome'
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>
-      <Head>
-        <title>NeXTIME 23</title>
-        <meta name="description" content="Site da empresa NeXTIME" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <NextButton>{'<Faça um orçamento gratuito/>'}</NextButton>
-        {/* <NextButton bg="primary-light">Primary Light</NextButton> */}
-        <NextButton>White</NextButton>
-        <NextButton bg="primary-dark">Dark</NextButton>
-      </main>
-
-      <footer>
-        <Image src="/images/logos/logo_nextime.svg" alt="NeXTIME Logo" width={146} height={45} />
-      </footer>
-    </div>
+    <TemplateHome />
   )
 }
+
+export default Home
