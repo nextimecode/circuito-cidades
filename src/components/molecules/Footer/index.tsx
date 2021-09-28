ce9119a5745e649b9a7d669fb1aa4f42349b55be
Pedro Duarte <pedrohlduarte@gmail.com>
2021-09-28 19:50:28 -0300

    refactor: footer

--- src/components/molecules/Footer/index.tsx
+++ src/components/molecules/Footer/index.tsx
@@ -1,48 +1,42 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Container, Flex, Text } from '@chakra-ui/react'
 import React, { PropsWithChildren } from 'react'
-import Footer from '../../atoms/Footer'
+import FooterAtoms from '../../atoms/Footer'
 import NextFotterBox from '../../atoms/NextFotterBox'
 
 type Props = {
   children?: string
 }
-const NextFotter = ({
+const Footer = ({
   children = ''
-}: PropsWithChildren<Props>) =>
+}: PropsWithChildren<Props>) => {
+  return (
+    <Container bg='#F7FAFC' maxW="container.xl">
+      <Box pt={5}>
+        <Box>
+          <Text fontSize={['24px', '36px']} color='primary'>
+            Realização
+          </Text>
+        </Box>
 
-  (
-    <Box
-      w={['360px', '100%']}
-      h={['100%', '380px']}
-      bg='#F7FAFC'
-    >
+        <Flex
+          direction={['column', 'column', 'row', 'row']}
+          rounded='lg'
+          p={5}
+        >
+          <NextFotterBox imageSrc="../images/footer/image1.png">Nova União - MG</NextFotterBox>
+          <NextFotterBox imageSrc="../images/footer/image2.png">João Monlevade - MG</NextFotterBox>
+          <NextFotterBox imageSrc="../images/footer/image3.png">Rio Piracicaba - MG</NextFotterBox>
+          <NextFotterBox imageSrc="../images/footer/image4.png"> S.D. Prata e Santa Maria - MG</NextFotterBox>
+          <NextFotterBox imageSrc="../images/footer/image5.png">São Gonçalo do Rio Abaixo - MG</NextFotterBox>
+          <NextFotterBox imageSrc="../images/footer/Vector.png">A definir (Grande final)</NextFotterBox>
+        </Flex>
 
-      <Box pt='48px' pl='83px'>
-        <Text fontSize={['24px', '36px']} color='red.600'>
-        Realização
-        </Text>
+        <FooterAtoms imageSrc="../images/footer/image6.png">APOIO</FooterAtoms>
+
+        {children}
+
       </Box>
-
-      <Flex
-        direction={['column', 'column', 'row', 'row']}
-        boxShadow='md'
-        rounded='lg'
-        p={'24px'}
-      >
-        <NextFotterBox imageSrc="../images/footer/image1.png">Nova União - MG</NextFotterBox>
-        <NextFotterBox imageSrc="../images/footer/image2.png">João Monlevade - MG</NextFotterBox>
-        <NextFotterBox imageSrc="../images/footer/image3.png">Rio Piracicaba - MG</NextFotterBox>
-        <NextFotterBox imageSrc="../images/footer/image4.png"> S.D. Prata e Santa Maria - MG</NextFotterBox>
-        <NextFotterBox imageSrc="../images/footer/image5.png">São Gonçalo do Rio Abaixo - MG</NextFotterBox>
-        <NextFotterBox imageSrc="../images/footer/Vector.png">A definir (Grande final)</NextFotterBox>
-
-      </Flex>
-
-      <Footer imageSrc="../images/footer/image6.png">Apoio</Footer>
-
-      {children}
-
-    </Box>
-
+    </Container>
   )
-export default NextFotter
+}
+export default Footer
