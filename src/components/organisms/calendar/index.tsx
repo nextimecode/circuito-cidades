53dfac8461bb1064ce8f8a60224ef39901c5fbc4
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-09-30 11:58:33 -0300

    fix: cria etapas no calendario de eventos

--- src/components/organisms/calendar/index.tsx
+++ src/components/organisms/calendar/index.tsx
@@ -3,16 +3,25 @@ import { Calendar, DateObject } from 'react-multi-date-picker'
 import 'react-multi-date-picker/styles/colors/red.css'
 import portugueseLowercase from '../../../utils/calendar_language'
 import { dates } from '../../../pages/api/dates'
+import Stages from '../../molecules/stages'
+import { Box, Flex } from '@chakra-ui/layout'
+import { useBreakpointValue } from '@chakra-ui/media-query'
 
 export default function EventCalendar() {
+  const smallDevice = useBreakpointValue({ base: true, md: false })
   return (
-    <Calendar
-      locale={portugueseLowercase}
-      className="red"
-      value={dates}
-      readOnly
-      currentDate={
-        new DateObject()}
-    />
+    <Flex justify='space-evenly' w='100%'>
+      <Box display={smallDevice ? 'none' : 'block'}>
+        <Calendar
+          locale={portugueseLowercase}
+          className="red"
+          value={dates}
+          readOnly
+          currentDate={
+            new DateObject()}
+        />
+      </Box>
+      <Stages />
+    </Flex>
   )
 }
