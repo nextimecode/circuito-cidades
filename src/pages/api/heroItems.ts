5c58664fc184d2f9d6645eaf66a2a52b4e303599
Pedro Duarte <pedrohlduarte@gmail.com>
2021-11-02 19:36:01 -0300

    feat: banners do hero

--- src/pages/api/heroItems.ts
+++ src/pages/api/heroItems.ts
@@ -3,7 +3,7 @@ export const heroItems = {
   heroDescription: `O Circuito Cidades é um evento esportivo de cooperação intermunicipal em caráter regional, exclusivo de modalidades individuais ou em duplas, que surgiu como alternativa para realização de atividades em meio a pandemia, fomentando a prática desportiva nas cidades e garantindo a segurança dos participantes, incluindo crianças, adolescentes, adultos e idosos.
   O evento será realizado em uma ou duas datas locais para cada cidade, sendo essas, fases classificatórias para a Etapa Final (a ser realizada no mês de dezembro de 2021).`,
   heroImages: [
-    '/images/heroItems/heroOP.jpeg',
+    '/images/heroItems/heroJM.jpeg',
     '/images/heroItems/heroPrata.jpeg',
     '/images/heroItems/heroNovaEra.jpeg',
     '/images/heroItems/heroBaraoCocais.jpeg'
