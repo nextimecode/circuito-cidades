39ca61da117a529e6644162da030673a16ef60c1
Pedro Duarte <pedrohlduarte@gmail.com>
2022-09-23 23:00:10 -0300

    feat: add eventos no calendario

--- src/components/organisms/calendar/index.tsx
+++ src/components/organisms/calendar/index.tsx
@@ -2,27 +2,36 @@ import React from 'react'
 import { Calendar, DateObject } from 'react-multi-date-picker'
 import 'react-multi-date-picker/styles/colors/red.css'
 import portugueseLowercase from '../../../utils/calendar_language'
-import { dates } from '../../../pages/api/dates'
 import Stages from '../../molecules/stages'
 import { Box, Flex } from '@chakra-ui/layout'
 import { useBreakpointValue } from '@chakra-ui/media-query'
+import { useGetDateEventQuery } from '../../../graphql/generated'
 
 const EventCalendar = () => {
   const smallDevice = useBreakpointValue({ base: true, md: false })
-  return (
-    <Flex justify="space-evenly" w="100%">
-      <Box display={smallDevice ? 'none' : 'block'}>
-        <Calendar
-          locale={portugueseLowercase}
-          className="red"
-          value={dates}
-          readOnly
-          currentDate={new DateObject()}
-        />
-      </Box>
-      <Stages />
-    </Flex>
-  )
+  const { data } = useGetDateEventQuery()
+  if (!data || !data.dateEvents[0].dates) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    )
+  } else {
+    return (
+      <Flex justify="space-evenly" w="100%">
+        <Box display={smallDevice ? 'none' : 'block'}>
+          <Calendar
+            locale={portugueseLowercase}
+            className="red"
+            value={data.dateEvents[0].dates}
+            readOnly
+            currentDate={new DateObject()}
+          />
+        </Box>
+        <Stages />
+      </Flex>
+    )
+  }
 }
 
 export default EventCalendar
