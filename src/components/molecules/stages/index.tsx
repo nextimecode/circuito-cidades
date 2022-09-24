1198178fbfd802c011d9189d8cfe28f1ef1b0be3
Pedro Duarte <pedrohlduarte@gmail.com>
2022-09-23 23:15:04 -0300

    feat: add etapas

--- src/components/molecules/stages/index.tsx
+++ src/components/molecules/stages/index.tsx
@@ -1,36 +1,43 @@
 import React from 'react'
-
+import { useGetEtapasQuery } from '../../../graphql/generated'
 import { Flex, Box, Text, VStack } from '@chakra-ui/react'
-import { stage } from '../../../pages/api/stage'
-
-const half = stage.length / 2
-const columns = [stage.slice(0, half), stage.slice(half, stage.length)]
 
 const Stages = () => {
-  return (
-    <Flex direction={['column', 'row']}>
-      {columns.map((column, indexColumn) => (
-        <Flex key={indexColumn} direction="column" px={[4, 8]}>
-          <VStack spacing={4}>
-            {column.map((stage, indexStage) => (
-              <Box as="div" key={indexStage} placeSelf="self-start">
-                <Text fontSize="sm" color="gray.500" py={2} placeSelf="self-start">
-                  {stage.date}
-                </Text>
-                <VStack as="div" spacing={2}>
-                  {stage.city.map((city, indexCity) => (
-                    <Text fontSize="lg" color="primary" key={indexCity} placeSelf="self-start">
-                      {city}
-                    </Text>
-                  ))}
-                </VStack>
-              </Box>
-            ))}
-          </VStack>
-        </Flex>
-      ))}
-    </Flex>
-  )
+  const { data } = useGetEtapasQuery()
+  if (!data || !data.etapas) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    )
+  } else {
+    const half = data.etapas.length / 2
+    const columns = [data.etapas.slice(0, half), data.etapas.slice(half, data.etapas.length)]
+    return (
+      <Flex direction={['column', 'row']}>
+        {columns.map((column, indexColumn) => (
+          <Flex key={indexColumn} direction="column" px={[4, 8]}>
+            <VStack spacing={4}>
+              {column.map((stage, indexStage) => (
+                <Box as="div" key={indexStage} placeSelf="self-start">
+                  <Text fontSize="sm" color="gray.500" py={2} placeSelf="self-start">
+                    {stage.date}
+                  </Text>
+                  <VStack as="div" spacing={2}>
+                    {stage.city.map((city, indexCity) => (
+                      <Text fontSize="lg" color="primary" key={indexCity} placeSelf="self-start">
+                        {city}
+                      </Text>
+                    ))}
+                  </VStack>
+                </Box>
+              ))}
+            </VStack>
+          </Flex>
+        ))}
+      </Flex>
+    )
+  }
 }
 
 export default Stages
